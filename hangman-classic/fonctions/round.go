4d7ed4856e304cf8f971a4cf936d1cc9187523da
mleo <leo.mace@ynov.com>
2022-11-14 10:37:11 +0100

    Ficher [Organisé] Hangman-Classic [Suppression des fonctions inutile]

--- hangman-classic/fonctions/round.go
+++ hangman-classic/fonctions/round.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func Round(mode, wordToFind, word string, vowelsCount, attempts int, ascii_letter, hangmanStep [][]string, useLettre []string, message string) bool {
+func Round(mode, wordToFind, word string, vowelsCount, attempts int, hangmanStep [][]string, useLettre []string, message string) bool {
 	/*
 	Input: Variable necessary for the game (Game mode, Word to find, the current state of the word, number of vowels already entered, the letters in ascci format, the steps of the hangman, the letters used, if the previous letter has already was used)
 	Performs and calls the different actions of the game (This function is recursive)
@@ -18,26 +18,8 @@ func Round(mode, wordToFind, word string, vowelsCount, attempts int, ascii_lette
 		return false
 	}else {
 		Clear()
-		fmt.Println(mode)
-		fmt.Println(message)
-		message = ""
-		switch mode {
-			case "psEasy":
-				RoundPisteNoire(mode, wordToFind, word, vowelsCount, attempts, hangmanStep, useLettre)
-				return false
-			case "psMedium":
-				RoundPisteNoire(mode, wordToFind, word, vowelsCount, attempts, hangmanStep, useLettre)
-				return false
-			case "psHard":
-				RoundPisteNoire(mode, wordToFind, word, vowelsCount, attempts, hangmanStep, useLettre)
-				return false
-			case "ascii-art":
-				AsciiArtScreen(ascii_letter, word, attempts)
-				DisplayHangman(hangmanStep, attempts)
-				fmt.Println()
-			default:
-				StandardScreen(word, attempts, hangmanStep, useLettre)
-		}
+
+		StandardScreen(word, attempts, hangmanStep, useLettre)
 	
 		input := RequestLetterWord()
 	
@@ -48,15 +30,8 @@ func Round(mode, wordToFind, word string, vowelsCount, attempts int, ascii_lette
 			return false
 		}
 	
-		/*if mode == "hard" {
-			vowelsCount = isVowel(input, vowelsCount)
-			if vowelsCount >= 3 {
-				attempts--
-			}
-		}*/
-	
 		fmt.Print("\n")
 		
-		return Round(mode, wordToFind, word, vowelsCount, attempts, ascii_letter, hangmanStep, useLettre, message)
+		return Round(mode, wordToFind, word, vowelsCount, attempts, hangmanStep, useLettre, message)
 	}
 }
--- hangman-classic/main.go
+++ hangman-classic/main.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"fmt"
 	hangman "hangman/fonctions"
 	"math/rand"
-	"os"
-	"io/ioutil"
-	"strconv"
 	"time"
-	"encoding/json"
 )
 
 func main() {
 	mode := "classique"
-
-	ascii_letter := [][]string{}
-
-	if hangman.Len(hangman.SliceSliceToSlice(ascii_letter)) == 0 {
-		for i := 0; i < hangman.Len(hangman.Read(".\\assets\\standard.txt")); i += 9 {
-			ascii_letter = append(ascii_letter, hangman.Read(".\\assets\\standard.txt")[i:i+9])
-		}
-	}
 
 	hangmanStep := [][]string{}
 
@@ -31,87 +18,11 @@ func main() {
 	word := ""
 	attempts := 10
 	useLettre := []string{}
-	message := ""
+	message := "Start"
 
 	rand.Seed(time.Now().UnixNano())
 
-	dictRoad := ""
-
-	switch hangman.Len(os.Args) {
-	case 1:
-		mode = "psMedium"
-		dictRoad = "./assets/words.txt"
-	case 2:
-		dictRoad = os.Args[1]
-		if _, err := os.Stat(os.Args[1]); err != nil {
-			fmt.Println("Please enter a valide file.")
-			return
-		}
-	case 3:
-		if os.Args[1] == "--startWith" {
-			if os.Args[2] == "save.txt" {
-				saveData, err := ioutil.ReadFile(".\\assets\\save.txt")
-				if err != nil {
-					panic(err)
-				}
-				var saveDataSlice []string
-				err = json.Unmarshal(saveData, &saveDataSlice)
-				if err != nil{
-					panic(err)
-				}
-				message = "-- Your save is start --"
-				attempts, _ := strconv.Atoi(saveDataSlice[3])
-				hangman.Round(saveDataSlice[0], saveDataSlice[2], saveDataSlice[1], 0, attempts, ascii_letter, hangmanStep, hangman.StringToSlice(saveDataSlice[4]), message)
-			}
-		}
-		if os.Args[2] == "--letterFile" {
-			fmt.Println("Please enter ascii letter file.")
-			return
-		}
-		if os.Args[1] == "--hard" {
-			dictRoad = os.Args[2]
-			if _, err := os.Stat(os.Args[2]); err != nil {
-				fmt.Println("Please enter a valide file.")
-				return
-			}
-			mode = "hard"
-		} else if os.Args[2] == "--hard" {
-			dictRoad = os.Args[1]
-			if _, err := os.Stat(os.Args[1]); err != nil {
-				fmt.Println("Please enter a valide file.")
-				return
-			}
-			mode = "hard"
-		} else {
-			fmt.Println("To many arguments")
-			return
-		}
-	case 4:
-		if os.Args[2] == "--letterFile"{
-			mode = "ascii-art"
-			dictRoad = os.Args[1]
-			if _, err := os.Stat(os.Args[3]); err != nil {
-				fmt.Println("Please enter a valide file.")
-				return
-			}
-			letterFileRoad := os.Args[3]
-			if _, err := os.Stat(os.Args[3]); err != nil {
-				fmt.Println("Please enter a valide file.")
-				return
-			}
-			ascii_letter = [][]string{}
-			for i := 0; i < hangman.Len(hangman.Read(letterFileRoad)); i += 9 {
-				ascii_letter = append(ascii_letter, hangman.Read(letterFileRoad)[i:i+9])
-			}
-
-		}else {
-			fmt.Println("To many arguments")
-			return
-		}
-	default:
-		fmt.Println("To many arguments")
-		return
-	}
+	dictRoad := "./assets/words.txt"
 
 	wordToFind := hangman.ReplaceAccentMaj(hangman.Read(dictRoad)[rand.Intn(hangman.Len(hangman.Read(dictRoad)))])
 
@@ -119,38 +30,7 @@ func main() {
 		word += "_"
 	}
 
-	switch mode {
-	case "hard":
-		word, useLettre = hangman.RandomReveal(hangman.Len(hangman.StringToSlice(wordToFind))/3-1, word, wordToFind)
-	default:
-		word, useLettre = hangman.RandomReveal(hangman.Len(hangman.StringToSlice(wordToFind))/2-1, word, wordToFind)
-	}
-	if _, err := os.Stat("assets/save.txt"); err == nil {
-		var reponse string
-		fmt.Println("We found a save.\nWant you to launch them ? [y/n]")
-		fmt.Scanln(&reponse)
-
-		switch reponse {
-		case "y":
-			saveData, err := ioutil.ReadFile(".\\assets\\save.txt")
-			if err != nil {
-				panic(err)
-			}
-			var saveDataSlice []string
-			err = json.Unmarshal(saveData, &saveDataSlice)
-			if err != nil {
-				panic(err)
-			}
-			attempts, _ := strconv.Atoi(saveDataSlice[3])
-			message = "-- Your save is start --"
-			hangman.Round(saveDataSlice[0], saveDataSlice[2], saveDataSlice[1], 0, attempts, ascii_letter, hangmanStep, hangman.StringToSlice(saveDataSlice[4]), message)
-			return
-		default:
-			hangman.Round(mode, wordToFind, word, 0, attempts, ascii_letter, hangmanStep, useLettre, message)
-			return
-		}
-
-	} else {
-		hangman.Round(mode, wordToFind, word, 0, attempts, ascii_letter, hangmanStep, useLettre, message)
-	}
+	word, useLettre = hangman.RandomReveal(hangman.Len(hangman.StringToSlice(wordToFind))/2-1, word, wordToFind)
+	
+	hangman.Round(mode, wordToFind, word, 0, attempts, hangmanStep, useLettre, message)
 }
